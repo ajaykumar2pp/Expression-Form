884af1f74b3d96352c273b517ee5770bc764d44b
ajaykumar2pp <ajay2kumarpp@gmail.com>
2024-01-24 16:44:24 +0530

    Expression Form

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Expression from './components/ExpressionForm';
+
+function App() {
+  return (
+    <div className='mb-5 mt-3'>
+      <h2 className="text-primary text-center mt-3 mb-5">Expression Form</h2>
+      <Expression />
+    </div>
+  );
+}
+
+export default App;
